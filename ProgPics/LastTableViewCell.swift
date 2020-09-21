85d031b774c4d3146e2c9080ab7eda268ee8d1dc
Erik Persson <erikpersson96@hotmail.com>
2020-09-21 14:36:02 +0200

    Ny Kategori knapp tillagd, segue till ny kategori view controller. Ersatte buttons i cellsen med tableview didselectrow.

---
+++ ProgPics/LastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LastTableViewCell.swift
+//  ProgPics
+//
+//  Created by Erik Persson on 2020-09-21.
+//
+
+import UIKit
+
+class LastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ProgPics/YourProgressionsViewController.swift
+++ ProgPics/YourProgressionsViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 
 class YourProgressionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
+    var numberOfCategories = 2
     @IBOutlet weak var progressionsTableView: UITableView!
-    var tableViewCells = [UserCategoryTableViewCell]()
+    //var tableViewCells = [UserCategoryTableViewCell]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,21 +26,45 @@ class YourProgressionsViewController: UIViewController, UITableViewDataSource, U
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return numberOfCategories + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        if (indexPath.row == numberOfCategories)
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "lastCell") as! LastTableViewCell
+            //tableViewCells.append(cell)
+            return cell
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell") as! UserCategoryTableViewCell
-        tableViewCells.append(cell)
+        //tableViewCells.append(cell)
         return cell
     }
     
-    @IBAction func cellButtonPress(_ sender: AnyObject) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.row == numberOfCategories)
+        {
+            performSegue(withIdentifier: "segue to new", sender: nil)
+        }
+        else
+        {
+            performSegue(withIdentifier: "segue to category", sender: nil)
+        }
+
+    }
+    
+    //@IBAction func cellButtonPress(_ sender: AnyObject) {
         //let buttonPosition:CGPoint = sender.convert(CGPoint.zero, to:self.progressionsTableView)
         //let indexPath = self.progressionsTableView.indexPathForRow(at: buttonPosition)
 
         
-        //performSegue(withIdentifier: "segue", sender: nil)
-    }
+        //performSegue(withIdentifier: "segue to category", sender: nil)
+    //}
+    
+    //@IBAction func newCategoryButton(_ sender: Any) {
+        
+        
+    //}
 }
