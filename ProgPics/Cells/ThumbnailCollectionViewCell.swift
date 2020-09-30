00ef22adea8533bedeb5f212138b1675139c2da0
Erik Persson <erikpersson96@hotmail.com>
2020-09-30 15:09:23 +0200

    collection view i gallery

---
+++ ProgPics/Cells/ThumbnailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ThumbnailCollectionViewCell.swift
+//  ProgPics
+//
+//  Created by Erik Persson on 2020-09-30.
+//
+
+import UIKit
+
+class ThumbnailCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- ProgPics/ViewControllers/GalleryViewController.swift
+++ ProgPics/ViewControllers/GalleryViewController.swift
@@ -7,24 +7,31 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
-
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
     var testString = "fail"
+    var numberOfPictures:Int?
     override func viewDidLoad() {
         super.viewDidLoad()
         print(testString)
+        numberOfPictures = 15
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "thumbnail cell" , for: indexPath) as! ThumbnailCollectionViewCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: Int(collectionView.frame.width/3) - 5, height: Int(collectionView.frame.width/3) - 5)
+    }
+    
 }
